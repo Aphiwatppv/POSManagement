f87cfb7e4609df3a70ba374570db11223efadf5f
Aphiwat Deelorm <AphiwatPPV@outlook.com>
2024-03-01 23:33:33 +0700

    Add Models to the folders DataAccess\Models

---
+++ POSManagement/DataAccess/Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Models;
+
+internal class Employee
+{
+    public int IdEmployee { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Role { get; set; }
+    public DateTime? DateOfBirth { get; set; }
+    public DateTime? DateOfEnroll { get; set; }
+    public int Age { get; set; }
+    public int Experience { get; set; }
+}
